d4e340196e682b31852b3ce3eddd9f374bff1d40
march-dave <ysangy@gmail.com>
2016-10-28 08:29:05 -0400

    up to date

--- src/app.js
+++ src/app.js
@@ -1,44 +1,60 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import { addTodo } from './actions/actions'
+import React from 'react';
 
-import ReactDOM from 'react-dom'
-import Contact from './components/Contact'
-import ContactDetails from './components/ContactDetails'
-
-import AddTodo from './components/AddTodo'
-import TodoList from './components/TodoList'
-
-// import Counter from './components/Counter'
-
-class App extends Component {
+class App extends React.Component {
 
   render() {
-    const { dispatch, visibleTodos } = this.props
-
-    return(
+    return (
       <div>
-
-          <AddTodo
-            onAddClick = {text =>
-            dispatch(addTodo(text))}
-            />
-          <TodoList todos = {visibleTodos} />
-          <ContactDetails />
-          This is the React App
-          <Contact title={this.props.headerTitle} myname={this.props.firstName} />
-
-
+          Hello World!!;
       </div>
     );
   }
 }
 
-function select(state) {
-  return (
-    visibleTodos: state.todos
-  )
-}
-export default connect(select)(App)
 
-ReactDOM.render(<App headerTitle="Ha Ha Ha" firstName="Dave" />, document.getElementById('app'))
+
+
+// import React, { Component } from 'react'
+// import { connect } from 'react-redux'
+// import { addTodo } from './actions/actions'
+//
+// import ReactDOM from 'react-dom'
+// import Contact from './components/Contact'
+// import ContactDetails from './components/ContactDetails'
+//
+// import AddTodo from './components/AddTodo'
+// import TodoList from './components/TodoList'
+//
+// // import Counter from './components/Counter'
+//
+// class App extends Component {
+//
+//   render() {
+//     const { dispatch, visibleTodos } = this.props
+//
+//     return(
+//       <div>
+//
+//           <AddTodo
+//             onAddClick = {text =>
+//             dispatch(addTodo(text))}
+//             />
+//           <TodoList todos = {visibleTodos} />
+//           <ContactDetails />
+//           This is the React App
+//           <Contact title={this.props.headerTitle} myname={this.props.firstName} />
+//
+//
+//       </div>
+//     );
+//   }
+// }
+//
+// function select(state) {
+//   return (
+//     visibleTodos: state.todos
+//   )
+// }
+// export default connect(select)(App)
+//
+// ReactDOM.render(<App headerTitle="Ha Ha Ha" firstName="Dave" />, document.getElementById('app'))
--- src/main.js
+++ src/main.js
@@ -1,41 +1,47 @@
-import React from 'react'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './app';
 
-import { render } from 'react-dom'
-import { createStore } from 'redux'
-import { Provider } from 'react-redux'
-import { Router, Route, Link, browerHistroy, IndexRoute } from 'react-rotuer'
+ReactDOM.render(<App />, document.getElementById('app'));
 
-import App from './app'
-import todoApp from './reducres/reducres'
-
-let store = creatStore(todoApp)
-let rootElement = document.getElementById('app')
-
-render (
-
-  <div>
-      <ul>
-          <li>Home</Link>
-          <li>About</Link>
-          <li>Contact</Link>
-      </ul>
-      {this.props.children}
-  </div>
-
-  <Provider store = {store}>
-    <App />
-  <Provider>,
-
-    rootElement
-)
-
-ReactDOM.render({
-  <Router histroy={browserHistory}>
-    <Router path="/" component={App}>
-      <IndexRoute component={Home} />
-      <Route paht="home" component={Home} />
-      <Route path="abput" component={About} />
-      <Route path="contact" component={Contact} />
-    </Router>
-  </Router>
-})
+// import React from 'react'
+//
+// import { render } from 'react-dom'
+// import { createStore } from 'redux'
+// import { Provider } from 'react-redux'
+// import { Router, Route, Link, browerHistroy, IndexRoute } from 'react-rotuer'
+//
+// import App from './app'
+// import todoApp from './reducres/reducres'
+//
+// let store = creatStore(todoApp)
+// let rootElement = document.getElementById('app')
+//
+// render (
+//
+//   <div>
+//       <ul>
+//           <li>Home</Link>
+//           <li>About</Link>
+//           <li>Contact</Link>
+//       </ul>
+//       {this.props.children}
+//   </div>
+//
+//   <Provider store = {store}>
+//     <App />
+//   <Provider>,
+//
+//     rootElement
+// )
+//
+// ReactDOM.render({
+//   <Router histroy={browserHistory}>
+//     <Router path="/" component={App}>
+//       <IndexRoute component={Home} />
+//       <Route paht="home" component={Home} />
+//       <Route path="abput" component={About} />
+//       <Route path="contact" component={Contact} />
+//     </Router>
+//   </Router>
+// })
