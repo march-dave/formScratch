f2670b294dd374bbec93aabafe8dbcfc023aaa34
march-dave <ysangy@gmail.com>
2016-12-02 06:25:17 -0500

    up to date:

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -17,32 +17,3 @@ class Todos extends Component {
   }
 
 }
-
-export default Todos;
-  // constructor(props) {
-  //   super(props);
-  //   this.state = { firstname:'', lastname:'', age:'' }
-  //   this.handleFirstNameChange = this.handleFirstNameChange.bind(this);
-  // }
-
-  // handleFirstNameChange(e) {
-  //   console.log('handleFirstNameChange', e.target);
-  //   this.setState( { firstname: e.target.value } );
-  // }
-
-  // render() {
-  //   return (
-  //     <form>
-  //         <div className="container-fluid">
-  //             <label>First Name</label><input onChange={this.handleFirstNameChange} value={this.state.firstname} />
-  //           </div>
-  //           <div>
-  //             <label>Last Name</label> <input value={this.state.lastname} />
-  //           </div>
-  //           <div>
-  //             <label>Age</label> <input value={this.state.age} />
-  //           </div>
-  //           <button>Click</button>
-  //     </form>
-  //   )
-  // }
