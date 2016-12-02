299c5fa3af87f4235781889134cace1bbd7a6729
march-dave <ysangy@gmail.com>
2016-12-01 19:58:50 -0500

    create props data flow with event

--- src/app.js
+++ src/app.js
@@ -3,11 +3,27 @@ import Todos from './components/Todos'
 
 class App extends React.Component {
 
+  constructor() {
+    super();
+
+    this.state = {
+      name: ''
+    }
+
+    this.updataState = this.updataState.bind(this);
+  }
+
+  updataState() {
+    this.setState( {
+      name: 'this is event'
+    })
+  }
+
   render() {
     return (
       <div>
 
-        <Todos />
+        <Todos firstname={this.state.name} updateProps={this.updataState}/>
 
       </div>
     );
--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,34 +1,48 @@
 import React, { Component } from 'react'
 
 class Todos extends Component {
-
-  constructor(props) {
-    super(props);
-    this.state = { firstname:'', lastname:'', age:'' }
-    this.handleFirstNameChange = this.handleFirstNameChange.bind(this);
-  }
-
-  handleFirstNameChange(e) {
-    console.log('handleFirstNameChange', e.target);
-    this.setState( { firstname: e.target.value } );
+  constructor() {
+    super();
   }
 
   render() {
-    return (
-      <form>
-          <div className="container-fluid">
-              <label>First Name</label><input onChange={this.handleFirstNameChange} value={this.state.firstname} />
-            </div>
-            <div>
-              <label>Last Name</label> <input value={this.state.lastname} />
-            </div>
-            <div>
-              <label>Age</label> <input value={this.state.age} />
-            </div>
-            <button>Click</button>
-      </form>
-    )
+    return(
+      <div>
+          <input value={this.props.firstname} />
+          <button onClick={this.props.updateProps}>
+            
+          </button>
+      </div>
+      );
   }
+
 }
 
 export default Todos;
+  // constructor(props) {
+  //   super(props);
+  //   this.state = { firstname:'', lastname:'', age:'' }
+  //   this.handleFirstNameChange = this.handleFirstNameChange.bind(this);
+  // }
+
+  // handleFirstNameChange(e) {
+  //   console.log('handleFirstNameChange', e.target);
+  //   this.setState( { firstname: e.target.value } );
+  // }
+
+  // render() {
+  //   return (
+  //     <form>
+  //         <div className="container-fluid">
+  //             <label>First Name</label><input onChange={this.handleFirstNameChange} value={this.state.firstname} />
+  //           </div>
+  //           <div>
+  //             <label>Last Name</label> <input value={this.state.lastname} />
+  //           </div>
+  //           <div>
+  //             <label>Age</label> <input value={this.state.age} />
+  //           </div>
+  //           <button>Click</button>
+  //     </form>
+  //   )
+  // }
