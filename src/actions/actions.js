9a8a32c7a255004afd54ad69129c2d9d53ed0362
march-dave <ysangy@gmail.com>
2016-12-04 16:11:42 -0500

    basic store

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -3,9 +3,9 @@ export const ADD_TODO = 'ADD_TODO'
 let nextTodoId = 0;
 
 export function addTodo(text) {
-  return {
-    type: ADD_TODO,
-    id: nextTodoId++,
-    text
-  };
+    return {
+        type: ADD_TODO,
+        id: nextTodoId++,
+        text
+    };
 }
--- src/main.js
+++ src/main.js
@@ -1,5 +1,18 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import App from './app';
-//
-// ReactDOM.render(<App />, document.getElementById('app'));
+import React from 'react'
+
+import { render } from 'react-dom'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+
+import App from './App'
+import todoApp from './reducres/reducers'
+
+let store = createStore(todoApp)
+
+render(
+    <Pvoider store={store}>
+        <App />
+    </Provider>,
+
+    app
+)
