e2fa05c519faf2cc1d3b25811bde7bd5214aedda
march-dave <ysangy@gmail.com>
2016-12-05 08:19:53 -0500

    Basic react-reduce Root Component

--- src/app.js
+++ src/app.js
@@ -1,33 +1,66 @@
-import React from 'react';
-import Todos from './components/Todos'
+import React, { Component } from 'react'
+import {connect} from 'react-redux'
+import {addTodo} from './actions/actions'
 
-class App extends React.Component {
+import AddTodo from './components/AddTodo'
+import TodoList from './components/TodoList'
 
-  constructor() {
-    super();
+class App extends Components {
+    render() {
+        const {dispatch, visibleTodos} = this.props
 
-    this.state = {
-      name: ''
+        return(
+            <div>
+                <AddTodo
+                    onAddClick = { text => 
+                        dispatch(addTodo(text))}
+                />
+
+                <TodoList tods = {VisibleTods} />
+            </div>
+        )
     }
-
-    this.updataState = this.updataState.bind(this);
-  }
-
-  updataState() {
-    this.setState( {
-      name: 'this is event'
-    })
-  }
-
-  render() {
-    return (
-      <div>
-
-        <Todos firstname={this.state.name} updateProps={this.updataState}/>
-
-      </div>
-    );
-  }
 }
 
-export default App;
+function select(state) {
+    return (
+        visibleTodos: state.todos
+    )
+}
+
+export default connect(select)(App)
+
+
+// import React from 'react';
+// import Todos from './components/Todos'
+
+// class App extends React.Component {
+
+//   constructor() {
+//     super();
+
+//     this.state = {
+//       name: ''
+//     }
+
+//     this.updataState = this.updataState.bind(this);
+//   }
+
+//   updataState() {
+//     this.setState( {
+//       name: 'this is event'
+//     })
+//   }
+
+//   render() {
+//     return (
+//       <div>
+
+//         <Todos firstname={this.state.name} updateProps={this.updataState}/>
+
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
