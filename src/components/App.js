d3dd166c1e0b28804686a316690f2b45e4507c85
march-dave <ysangy@gmail.com>
2016-12-07 08:39:26 -0500

    State

--- src/components/App.js
+++ src/components/App.js
@@ -5,13 +5,28 @@ import Option from './Option';
 import Contact from './Contact';
 
 class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      conactName: 'Contact Me'
+    }
+    // this.onClickIncrement = this.onClickIncrement.bind(this);
+  }
+
+  // onClickIncrement(e) {
+  //   this.setState({
+  //     conactName: e.target.value
+  //   })
+  // }
+
     render(){
         return (
             <div style={ {textAlign: 'center'} }>
                 <Counter/>
                 <Option/>
                 <Buttons/>
-                <Contact />
+                <Contact myname={this.state.conactName}/>
             </div>
         );
     }
